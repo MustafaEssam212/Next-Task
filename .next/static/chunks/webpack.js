3fda63a3389907c932403fd71118b08fec01efed
AhmedMagdy79 <ahmedmagdy35731@gmail.com>
2023-05-03 19:44:59 +0300

    Ahmed Magdy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c2d09daab1b2caca"; }
+/******/ 		__webpack_require__.h = function() { return "b6a0279e9d3e9dae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import dbConnect from '../../utiles/dbConnect';
+// hossam
+// import dbConnect from "../../utiles/dbConnect";
+
+// export default async function handler(req, res) {
+//     try {
+//         await dbConnect();
+//         console.log("Database connected successfully");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+//Ahmed Magdy
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const mongodb = require("mongodb");
+import clientPromise from "../../util/db.connect";
 
 export default async function handler(req, res) {
- try {
-    await dbConnect();
-    console.log('Database connected successfully');
- } catch (error) {
-  console.log(error);
- }
+    const client = await clientPromise;
+    const db = client.db("next_test");
+    // let myPost = await db.collection("test").insertOne({name:"mego"});
+    // console.log(myPost);
+    res.status(200).json({ message: "Database connected successfully" });
 }
