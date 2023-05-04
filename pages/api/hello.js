1dbbbe7c78b161259bb68a967079c33aa2017e12
hossamMazroa <ha7455903@gmail.com>
2023-05-04 05:27:48 +0200

    hossamMazroa

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import dbConnect from '../../utiles/dbConnect';
+
+export default async function handler(req, res) {
+ try {
+    await dbConnect();
+    console.log('Database connected successfully');
+ } catch (error) {
+  console.log(error);
+ }
 }
